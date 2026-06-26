4e501f9fdcc8b8fd8e2c3271532bce55dd86f904
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 16:49:06 -0700

    Implement Logger class with CSV saving functionality

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -1,1 +1,21 @@
+#include "utils/Logger.hpp"
 
+namespace sim {
+namespace utils {
+
+void Logger::log(double t, double r, double e, double u, double y) {
+    data.push_back({t, r, e, u, y});
+}
+
+void Logger::saveToCSV(const std::string& filename) const {
+    std::ofstream file(filename);
+    file << "Time,Setpoint,Error,Control,Output\n";
+    for (const auto& d : data) {
+        file << d.t << "," << d.setpoint << "," << d.error << "," << d.control_u << "," << d.state_y << "\n";
+    }
+}
+
+const std::vector<DataPoint>& Logger::getData() const { return data; }
+
+}
+}
